292c7d50ea535fe88b513e3086cb5fe5790c7214
theotrin <theopinheiro12@gmail.com>
2024-06-13 14:30:23 -0400

    first commit

---
+++ src/main/java/br/com/theotrin/deploy_2024/Deploy2024Application.java
@@ -0,0 +1,13 @@
+package br.com.theotrin.deploy_2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Deploy2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Deploy2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/theotrin/deploy_2024/Deploy2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.theotrin.deploy_2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Deploy2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
